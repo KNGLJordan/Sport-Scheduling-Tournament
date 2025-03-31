c7b49f9787d0caf4b4c4a905988c0600a4353813
KNGLJordan <giordani.francesco2002@gmail.com>
2025-03-31 17:36:01 +0200

    example for NQueens in SAT

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CDMO-project
+
+## res-demo
+
+- It containes a demo version of the project based on the N-Queens problem.
+- The purpose is to have a guideline for the real project.
---
+++ src_demo/SAT/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+
+
+def display_nqueens(sol):
+    board = [[0] * len(sol) for i in range(len(sol))]
+    for x, y in sol:
+        board[x][y] = 1
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            symbol = '♛' if board[i][j] == 1 else '.'
+            print(symbol, end=' ')
+        print()
