4a0eb4f40667a82b5f54a752c03c13d62ff12c33
Federico Hrvatin <federicohrvatin@gmail.com>
2025-08-29 12:16:23 +0200

    entrypoint for smt

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-MODE=$1       # CP, SAT, MIP oppure vuoto
+MODE=$1       # CP, SAT, SMT, MIP oppure vuoto
 INITIAL_N=$2  # opzionale
 FINAL_N=$3    # opzionale
 MODEL_NAME=$4 # opzionale
@@ -48,6 +48,28 @@ check_sat() {
     cd -
 }
 
+run_smt() {
+    echo "== Running SMT Solver =="
+    cd src/SMT
+    CMD="python3 solver.py"
+    [ -n "$INITIAL_N" ] && CMD="$CMD --initial_n $INITIAL_N"
+    [ -n "$FINAL_N" ] && CMD="$CMD --final_n $FINAL_N"
+    [ -n "$MODEL_NAME" ] && CMD="$CMD --modelname $MODEL_NAME"
+    [ -n "$PROBLEM_TYPE" ] && CMD="$CMD --problem_type $PROBLEM_TYPE"
+    echo "Running: $CMD"
+    eval $CMD
+    cd -
+}
+
+check_smt() {
+    echo "== Checking SMT Solver =="
+    cd src
+    CMD="python3 solution_checker.py ../res/SMT"
+    echo "Running: $CMD"
+    eval $CMD
+    cd -
+}
+
 run_mip() {
     echo "== Running MIP Solver =="
     cd src/MIP
@@ -76,10 +98,13 @@ if [ -z "$MODE" ]; then
     check_cp
     run_sat
     check_sat
+    run_smt
+    check_smt
     run_mip
     check_mip
 else
     [[ "$MODE" == "CP" ]] && run_cp && check_cp
     [[ "$MODE" == "SAT" ]] && run_sat && check_sat
+    [[ "$MODE" == "SMT" ]] && run_smt && check_smt
     [[ "$MODE" == "MIP" ]] && run_mip && check_mip
 fi
